1187191d1d1c3d32cc2dc1ffea43c9cef1c96e72
D-Modi <126412094+D-Modi@users.noreply.github.com>
2025-11-25 18:11:18 +0530

    Add default model name to create_model function
    Set a default model name and update create_model function to use it.

--- pii_ner_assignment_IITB/src/model.py
+++ pii_ner_assignment_IITB/src/model.py
@@ -2,11 +2,22 @@ from transformers import AutoModelForTokenClassification
 from labels import LABEL2ID, ID2LABEL
 
 
-def create_model(model_name: str):
-    model = AutoModelForTokenClassification.from_pretrained(
+DEFAULT_MODEL_NAME = "prajjwal1/bert-tiny"
+
+def create_model(model_name: str = DEFAULT_MODEL_NAME):
+    config = AutoConfig.from_pretrained(
         model_name,
         num_labels=len(LABEL2ID),
         id2label=ID2LABEL,
         label2id=LABEL2ID,
     )
+
+    config.output_attentions = False
+    config.output_hidden_states = False
+
+    model = AutoModelForTokenClassification.from_pretrained(
+        model_name,
+        config=config,
+    )
     return model
+
